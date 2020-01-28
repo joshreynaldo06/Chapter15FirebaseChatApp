2194f80a3ec7433b3e2e46d672ef07122790f936
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-28 15:42:57 +0700

    add constants file to save all string values

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Joshua Citra on 28/01/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Constants{
+    static let registerSegue = "registerVC"
+    static let loginSegue = "loginVC"
+}
