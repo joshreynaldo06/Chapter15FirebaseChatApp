54c8176dd27f6dfc6b3539b0304f95143ead43b7
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-28 14:53:16 +0700

    add firebase pod and login and logout features

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
@@ -15,11 +16,22 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logOutClicked(_ sender: UIBarButtonItem) {
+        
+        do{
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+            
+        } catch let signOutError as NSError{
+            print("Error when signing out: %@", signOutError)
+        }
+    }
+    
 }
